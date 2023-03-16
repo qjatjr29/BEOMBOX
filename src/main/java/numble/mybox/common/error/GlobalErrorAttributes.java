05ff0f8dbb72b97749a688e156eeee4c52d4edc1
beomsic <qjatjr29@khu.ac.kr>
2023-03-17 08:58:34 +0900

    :sparkles: feat: Exception 처리를 위한 Error code관련 클래스 추가

---
+++ src/main/java/numble/mybox/common/error/GlobalErrorAttributes.java
@@ -0,0 +1,31 @@
+package numble.mybox.common.error;
+
+import java.util.Map;
+import numble.mybox.common.error.exception.BusinessException;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+@Component
+public class GlobalErrorAttributes extends DefaultErrorAttributes {
+
+  @Override
+  public Map<String, Object> getErrorAttributes(ServerRequest request,
+      ErrorAttributeOptions options) {
+    Map<String, Object> errorAttributes = super.getErrorAttributes(request, options);
+
+    Throwable throwable = getError(request);
+
+    if(throwable instanceof BusinessException) {
+      BusinessException ex = (BusinessException) getError(request);
+      errorAttributes.put("exception", ex.getClass().getSimpleName());
+      errorAttributes.put("message", ex.getErrorCode().getMessage());
+      errorAttributes.put("status", ex.getErrorCode().getStatusCode());
+      errorAttributes.put("errorCode", ex.getErrorCode().getResultCode());
+    }
+
+    return errorAttributes;
+  }
+
+}
