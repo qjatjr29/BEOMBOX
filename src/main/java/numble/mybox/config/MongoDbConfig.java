3e8a63eba6d1423a6ef17595270d4d76931c6130
beomsic <qjatjr29@khu.ac.kr>
2023-03-17 08:57:09 +0900

    :fire: remove: 사용하지 않는 import 삭제

--- src/main/java/numble/mybox/config/MongoDbConfig.java
+++ src/main/java/numble/mybox/config/MongoDbConfig.java
@@ -1,9 +1,7 @@
 package numble.mybox.config;
 
-import com.mongodb.ConnectionString;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
