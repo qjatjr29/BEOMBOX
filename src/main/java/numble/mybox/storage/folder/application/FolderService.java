a7011d6fce96c078d80aa2317712f910f98b2dcd
beomsic <qjatjr29@khu.ac.kr>
2023-03-17 15:43:01 +0900

    :sparkles: feat: 폴더 service

---
+++ src/main/java/numble/mybox/storage/folder/application/FolderService.java
@@ -0,0 +1,67 @@
+package numble.mybox.storage.folder.application;
+
+import static java.util.Objects.isNull;
+
+import java.time.LocalDateTime;
+import numble.mybox.common.error.ErrorCode;
+import numble.mybox.common.error.exception.ConflictException;
+import numble.mybox.common.event.SignupCompletedEvent;
+import numble.mybox.storage.folder.domain.Folder;
+import numble.mybox.storage.folder.domain.FolderRepository;
+import numble.mybox.user.user.domain.UserTokenData;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@Transactional(readOnly = true)
+public class FolderService {
+
+  private final FolderRepository folderRepository;
+
+  public FolderService(FolderRepository folderRepository) {
+    this.folderRepository = folderRepository;
+  }
+
+  @Transactional
+  public Mono<FolderDetailResponse> createFolder(UserTokenData user, CreateFolderRequest request) {
+
+    return folderRepository.findByParentIdAndTitle(request.getParentFolderId(), request.getTitle())
+        .flatMap(existedFolder -> {
+          if (!isNull(existedFolder)) {
+            throw new ConflictException(ErrorCode.DUPLICATE_FOLDER);
+          } else {
+            Folder folder = Folder.builder()
+                .userId(user.getId())
+                .parentId(request.getParentFolderId())
+                .title(request.getTitle())
+                .updatedAt(LocalDateTime.now())
+                .build();
+            Mono<Folder> save = folderRepository.save(folder);
+            existedFolder.addSubFolder(save);
+            return save.flatMap(f -> Mono.just(FolderDetailResponse.of(f)));
+          }
+        });
+  }
+
+  public Flux<FolderSummaryResponse> findAllSubFolder(String folderId) {
+
+    Flux<Folder> folderList = folderRepository.findAllByParentId(folderId);
+
+    return folderList.flatMap(FolderSummaryResponse::of);
+  }
+
+  @Transactional
+  public Mono<Folder> createRootFolder(SignupCompletedEvent event) {
+
+    Folder rootFolder = Folder.builder()
+        .userId(event.getUserId())
+        .title(event.getTitle())
+        .isRoot(Boolean.TRUE)
+        .updatedAt(LocalDateTime.now())
+        .build();
+
+    return folderRepository.save(rootFolder);
+  }
+}
