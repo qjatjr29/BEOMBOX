c6f77304baec118c8bdb9f220240d3bd46514b93
beomsic <qjatjr29@khu.ac.kr>
2023-03-17 15:43:36 +0900

    :sparkles: feat: 폴더 생성 dto 추가

---
+++ src/main/java/numble/mybox/storage/folder/application/CreateFolderRequest.java
@@ -0,0 +1,20 @@
+package numble.mybox.storage.folder.application;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CreateFolderRequest {
+
+  @NotBlank
+  private String title;
+
+  @NotNull
+  private String parentFolderId;
+}
