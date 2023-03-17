b2fb7b8698ab9160843a12df8165f49c2d28b8e1
beomsic <qjatjr29@khu.ac.kr>
2023-03-17 15:31:40 +0900

    :sparkles: feat: 회원가입 이벤트 리스너 추가

---
+++ src/main/java/numble/mybox/storage/folder/infrastructure/SignupCompletedEventListener.java
@@ -0,0 +1,40 @@
+package numble.mybox.storage.folder.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import numble.mybox.common.event.SignupCompletedEvent;
+import numble.mybox.storage.folder.application.FolderService;
+import numble.mybox.storage.folder.domain.Folder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SignupCompletedEventListener {
+
+  private final FolderService folderService;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  public SignupCompletedEventListener(
+      FolderService folderService) {
+    this.folderService = folderService;
+  }
+
+  @KafkaListener(topics = "${spring.kafka.topic.signup-completed}")
+  public void consume(String message) {
+
+    try {
+      SignupCompletedEvent event = objectMapper.readValue(message, SignupCompletedEvent.class);
+      folderService.createRootFolder(event).subscribe();
+    } catch (IOException e) {
+      log.error("SignupCompletedEvent parsing error!");
+      throw new RuntimeException();
+    }
+  }
+
+}
