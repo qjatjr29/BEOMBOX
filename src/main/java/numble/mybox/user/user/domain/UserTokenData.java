145df128b3581e523cee967958485605d51da288
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:20:09 +0900

    :sparkles: feat: user에 대한 Jwt 토큰 정보에 id 타입 변경 및 role 추가

--- src/main/java/numble/mybox/user/user/domain/UserTokenData.java
+++ src/main/java/numble/mybox/user/user/domain/UserTokenData.java
@@ -8,22 +8,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserTokenData {
 
-  private String id;
+  private Long id;
   private String email;
   private String provider;
+  private String role;
 
-  public UserTokenData(String id, String email, String provider) {
+  private UserTokenData(Long id, String email, String provider, String role) {
     this.id = id;
     this.email = email;
     this.provider = provider;
+    this.role = role;
   }
 
   public static UserTokenData of(final User user) {
-    return new UserTokenData(user.getId(), user.getEmail(), user.getProvider());
+    return new UserTokenData(user.getId(), user.getEmail(), user.getProvider().getProviderType(), user.getRole().getRole());
   }
 
-  public static UserTokenData from(final String id, final String email, final String provider) {
-    return new UserTokenData(id, email, provider);
+  public static UserTokenData from(final Long id, final String email, final String provider, final String role) {
+    return new UserTokenData(id, email, provider, role);
   }
 
 }
