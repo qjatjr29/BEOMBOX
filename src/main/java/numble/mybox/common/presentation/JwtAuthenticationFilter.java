5acd25d91cee3ca6bdc63787e2839500bb28be5b
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:36:00 +0900

    :recycle: refactor: jwt 권한 filter mvc 패턴으로 변경

--- src/main/java/numble/mybox/common/presentation/JwtAuthenticationFilter.java
+++ src/main/java/numble/mybox/common/presentation/JwtAuthenticationFilter.java
@@ -1,16 +1,26 @@
 package numble.mybox.common.presentation;
 
-import numble.mybox.user.user.infrastructure.JwtProvider;
+import java.io.IOException;
+import java.util.Arrays;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import numble.mybox.user.user.domain.Role;
 import numble.mybox.user.user.domain.UserTokenData;
-import org.springframework.http.HttpHeaders;
+import numble.mybox.user.user.infrastructure.JwtProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 
 @Component
-public class JwtAuthenticationFilter implements WebFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private static final String HEADER_AUTHORIZATION = "Authorization";
 
   private final JwtProvider jwtProvider;
 
@@ -19,13 +29,23 @@ public class JwtAuthenticationFilter implements WebFilter {
   }
 
   @Override
-  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
 
-    String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-    if (token != null) {
 
+    String token = request.getHeader(HEADER_AUTHORIZATION);
+
+    if(token != null && token.length() != 0) {
       jwtProvider.validateToken(token);
+      UserTokenData tokenData = jwtProvider.getTokenData(token);
+      Authentication auth = getAuthentication(tokenData);
+      SecurityContextHolder.getContext().setAuthentication(auth);
     }
-    return chain.filter(exchange);
+    filterChain.doFilter(request, response);
+  }
+
+  public Authentication getAuthentication(UserTokenData tokenData) {
+    return new UsernamePasswordAuthenticationToken(tokenData, "",
+        Arrays.asList(new SimpleGrantedAuthority(Role.USER.getRole())));
   }
 }
