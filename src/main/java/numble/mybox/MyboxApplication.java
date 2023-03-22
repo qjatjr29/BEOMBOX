39aa123c6d7cc937babd52002e0194a7b7c68251
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:37:02 +0900

    :sparkles: feat: 서버 시간 설정 및 JpaAuditing 기능 사용하도록 기능 추가

--- src/main/java/numble/mybox/MyboxApplication.java
+++ src/main/java/numble/mybox/MyboxApplication.java
@@ -1,10 +1,19 @@
 package numble.mybox;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyboxApplication {
+
+	@PostConstruct
+	public void start() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyboxApplication.class, args);
