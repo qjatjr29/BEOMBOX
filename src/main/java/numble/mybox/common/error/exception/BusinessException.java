40594606d56db9192d12cdca7224eae9d10ac60b
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:27:02 +0900

    :sparkles: feat: error 정보를 처리하는 로직 추가

--- src/main/java/numble/mybox/common/error/exception/BusinessException.java
+++ src/main/java/numble/mybox/common/error/exception/BusinessException.java
@@ -1,13 +1,23 @@
 package numble.mybox.common.error.exception;
 
+import java.util.List;
+import lombok.Getter;
 import numble.mybox.common.error.ErrorCode;
+import numble.mybox.common.error.ErrorField;
 
+@Getter
 public class BusinessException extends RuntimeException {
 
   private final ErrorCode errorCode;
+  private List<ErrorField> errors;
 
   public BusinessException(ErrorCode errorCode) {
     this.errorCode = errorCode;
+  }
+
+  public BusinessException(ErrorCode errorCode, List<ErrorField> errors) {
+    this.errorCode = errorCode;
+    this.errors = errors;
   }
 
   public ErrorCode getErrorCode() {
