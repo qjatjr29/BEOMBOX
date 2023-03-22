ae46b3eaa35807ebe9dec7dbd6c50f0e55e52c5d
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:52:14 +0900

    :sparkles: feat: oauth2 successhandler 추가

---
+++ src/main/java/numble/mybox/user/user/application/OAuth2SuccessHandler.java
@@ -0,0 +1,54 @@
+package numble.mybox.user.user.application;
+
+import static java.util.Objects.isNull;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import numble.mybox.user.user.domain.UserTokenData;
+import numble.mybox.user.user.infrastructure.JwtProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+  private final JwtProvider jwtProvider;
+  private final ObjectMapper objectMapper;
+
+  public OAuth2SuccessHandler(JwtProvider jwtProvider,
+      ObjectMapper objectMapper) {
+    this.jwtProvider = jwtProvider;
+    this.objectMapper = objectMapper;
+  }
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) throws IOException, ServletException {
+
+    OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+    Map<String, Object> attributes = oAuth2User.getAttributes();
+
+    String registrationId = (String) attributes.get("provider");
+
+    UserTokenData tokenData = OAuth2TokenDataFactory.getTokenData(registrationId, oAuth2User);
+
+    if(!isNull(tokenData)) {
+      String accessToken = jwtProvider.generateAccessToken(tokenData);
+      String refreshToken = jwtProvider.generateRefreshToken(tokenData);
+
+      // TODO: refreshToken을 Redis에 저장해 관리하기.
+
+      AuthResponse authResponse = new AuthResponse(accessToken, refreshToken);
+
+      // 응답 본문에 토큰 정보 추가
+      String res = objectMapper.writeValueAsString(authResponse);
+      response.getWriter().write(res);
+    }
+  }
+}
