92ac33faf7883f4b63f17fd8e8b9ea097a724d23
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:30:07 +0900

    :fire: remove: 적용되지 않는 어노테이션 삭제

--- src/main/java/numble/mybox/common/domain/BaseEntity.java
+++ src/main/java/numble/mybox/common/domain/BaseEntity.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @MappedSuperclass
@@ -18,12 +17,10 @@ public class BaseEntity implements Serializable {
 
   @CreatedDate
   @Column(name = "created_at", updatable = false)
-  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime createdAt;
 
   @LastModifiedDate
   @Column(name = "updated_at", updatable = true)
-  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime updatedAt;
 
 
