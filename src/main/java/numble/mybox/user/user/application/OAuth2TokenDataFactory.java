7c85811d96a206ed4c1d8c0bb11cd874f32b6532
beomsic <qjatjr29@khu.ac.kr>
2023-03-22 13:48:13 +0900

    :recycle: refactor: jwt 토큰에 저장할 데이터변경에 따라 userId 타입을 long으로 변경

--- src/main/java/numble/mybox/user/user/application/OAuth2TokenDataFactory.java
+++ src/main/java/numble/mybox/user/user/application/OAuth2TokenDataFactory.java
@@ -1,8 +1,10 @@
 package numble.mybox.user.user.application;
 
 import java.util.Map;
+import numble.mybox.common.error.ErrorCode;
+import numble.mybox.common.error.exception.BadRequestException;
 import numble.mybox.user.user.domain.AuthProvider;
-import numble.mybox.user.user.domain.UserRepository;
+import numble.mybox.user.user.domain.Role;
 import numble.mybox.user.user.domain.UserTokenData;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
@@ -10,42 +12,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class OAuth2TokenDataFactory {
 
-  private final UserRepository userRepository;
-
-  public OAuth2TokenDataFactory(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
-
   public static UserTokenData getTokenData(String registrationId, OAuth2User principal) {
 
     if(registrationId.toUpperCase().equals(AuthProvider.GOOGLE.getProviderType())) {
 
-      String userId = principal.getAttribute("userId");
+      Long userId = principal.getAttribute("userId");
       String email = principal.getAttribute("email");
 
-      return UserTokenData.from(userId, email, AuthProvider.GOOGLE.getProviderType());
+      return UserTokenData.from(userId, email, AuthProvider.GOOGLE.getProviderType(), Role.USER.getRole());
 
     }
     else if(registrationId.toUpperCase().equals(AuthProvider.KAKAO.getProviderType())) {
 
       Map<String, Object> acounts = (Map<String, Object>) principal.getAttributes().get("kakao_account");
 
-      String userId = principal.getAttribute("userId");
+      Long userId = principal.getAttribute("userId");
       String email = String.valueOf(acounts.get("email"));
 
-      return UserTokenData.from(userId, email, AuthProvider.KAKAO.getProviderType());
+      return UserTokenData.from(userId, email, AuthProvider.KAKAO.getProviderType(), Role.USER.getRole());
     }
 
     else if(registrationId.toUpperCase().equals(AuthProvider.NAVER.getProviderType())) {
       Map<String, Object> response = (Map<String, Object>) principal.getAttribute("response");
 
-      String userId = principal.getAttribute("userId");
+      Long userId = principal.getAttribute("userId");
       String email = String.valueOf(response.get("email"));
 
-      return UserTokenData.from(userId, email, AuthProvider.NAVER.getProviderType());
+      return UserTokenData.from(userId, email, AuthProvider.NAVER.getProviderType(), Role.USER.getRole());
     }
 
-    return null;
+    throw new BadRequestException(ErrorCode.PROVIDER_NOT_SUPPORTED);
   }
 
 }
