0f8a1c92ed48da10152d2ce9a79aee0249ceea4a
beomsic <qjatjr29@khu.ac.kr>
2023-04-29 23:56:36 +0900

    :sparkles: feat: Mongo 관련 Properties 클래스 추가

---
+++ src/main/java/numble/mybox/config/properties/MongoProperties.java
@@ -0,0 +1,26 @@
+package numble.mybox.config.properties;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties("spring.data.mongodb")
+@Validated
+@Getter
+@Setter
+public class MongoProperties {
+
+  @NotEmpty
+  private String uri;
+  @NotEmpty
+  private String database;
+  @NotEmpty
+  private String username;
+  @NotEmpty
+  private String password;
+
+}
