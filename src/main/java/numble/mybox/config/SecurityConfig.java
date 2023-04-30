99c7702e7a570d374594625eaeb3472b28fe4b78
beomsic <qjatjr29@khu.ac.kr>
2023-04-30 15:55:01 +0900

    :sparkles: feat: webflux를 사용한 security config 클래스 추가

--- src/main/java/numble/mybox/config/SecurityConfig.java
+++ src/main/java/numble/mybox/config/SecurityConfig.java
@@ -1,62 +1,53 @@
 package numble.mybox.config;
 
+import numble.mybox.common.error.ErrorCode;
+import numble.mybox.common.error.exception.BusinessException;
 import numble.mybox.common.presentation.JwtAuthenticationFilter;
-import numble.mybox.user.user.application.CustomOAuth2UserService;
-import numble.mybox.user.user.application.OAuth2SuccessHandler;
-import numble.mybox.user.user.domain.Role;
+import numble.mybox.user.user.application.CustomOAuth2SuccessHandler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
 
-  private final OAuth2SuccessHandler oAuth2SuccessHandler;
-  private final CustomOAuth2UserService customOAuth2UserService;
+  private final CustomOAuth2SuccessHandler oAuth2SuccessHandler;
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   public SecurityConfig(
-      OAuth2SuccessHandler oAuth2SuccessHandler,
-      CustomOAuth2UserService customOAuth2UserService,
+      CustomOAuth2SuccessHandler oAuth2SuccessHandler,
       JwtAuthenticationFilter jwtAuthenticationFilter) {
     this.oAuth2SuccessHandler = oAuth2SuccessHandler;
-    this.customOAuth2UserService = customOAuth2UserService;
     this.jwtAuthenticationFilter = jwtAuthenticationFilter;
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
+  public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity serverHttpSecurity) {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        .httpBasic().disable()
+    return serverHttpSecurity
+        .cors().and()
         .csrf().disable()
-        .cors()
+        .httpBasic().disable()
+        .formLogin().disable()
+        .authorizeExchange()
+        .pathMatchers("/api/**").authenticated()
+        .pathMatchers("/users").authenticated()
+        .anyExchange().permitAll()
         .and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeRequests()
-        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-        .antMatchers("/users/**").hasRole(Role.USER.name())
-        .antMatchers("/folders/**").hasRole(Role.USER.name())
-        .antMatchers("/files/**").hasRole(Role.USER.name())
-        .anyRequest().permitAll()
+        .oauth2Login()
+        .authenticationSuccessHandler(oAuth2SuccessHandler)
         .and()
         .logout()
+        .and().exceptionHandling()
+        .accessDeniedHandler((exchange, exception) -> Mono.error(new BusinessException(ErrorCode.UNAUTHORIZED)))
         .and()
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-        .oauth2Login()
-        .successHandler(oAuth2SuccessHandler)
-        .userInfoEndpoint()
-        .userService(customOAuth2UserService);
+        .addFilterBefore(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+        .build();
   }
 
 }
