0445c746d07c5ec545b000c5a48aed3f35eee194
beomsic <qjatjr29@khu.ac.kr>
2023-04-30 16:25:11 +0900

    :recycle: refactor: file upload 컨트롤러 코드 수정

--- src/main/java/numble/mybox/storage/file/presentation/FileController.java
+++ src/main/java/numble/mybox/storage/file/presentation/FileController.java
@@ -1,33 +1,39 @@
 package numble.mybox.storage.file.presentation;
 
 import numble.mybox.common.presentation.CurrentUser;
+import numble.mybox.storage.file.application.AwsS3Service;
 import numble.mybox.storage.file.application.FileDetailResponse;
 import numble.mybox.storage.file.application.FileService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
 
   private final FileService fileService;
+  private final AwsS3Service awsS3Service;
 
-  public FileController(FileService fileService) {
+  public FileController(FileService fileService,
+      AwsS3Service awsS3Service) {
     this.fileService = fileService;
+    this.awsS3Service = awsS3Service;
   }
 
-  @PostMapping("/{folderId}")
-  public ResponseEntity<FileDetailResponse> uploadFile(@CurrentUser Long userId,
-      @PathVariable Long folderId,
-      @RequestParam("file") MultipartFile file) {
-    FileDetailResponse response = fileService.uploadFile(userId, folderId, file);
-
-    return ResponseEntity.ok(response);
+  @PostMapping("/upload/{folderId}")
+  public Mono<ResponseEntity<FileDetailResponse>> uploadFile(
+      @CurrentUser String userId,
+      @PathVariable String folderId,
+      @RequestPart("file") FilePart file) {
+    return fileService.uploadFile(userId, folderId, file).map(ResponseEntity::ok);
   }
+
+
 
 }
