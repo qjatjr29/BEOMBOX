a27ddc884169e51c31b83267cba91687b25250e3
beomsic <qjatjr29@khu.ac.kr>
2023-04-30 15:45:40 +0900

    :recycle: refactor: 유저 정보 조회 컨트롤러 수정

--- src/main/java/numble/mybox/user/user/presentation/UserController.java
+++ src/main/java/numble/mybox/user/user/presentation/UserController.java
@@ -1,16 +1,13 @@
 package numble.mybox.user.user.presentation;
 
-import javax.validation.Valid;
 import numble.mybox.common.presentation.CurrentUser;
-import numble.mybox.user.user.application.UpdateRequest;
 import numble.mybox.user.user.application.UserService;
 import numble.mybox.user.user.application.UserDetailResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 
 @RestController
@@ -24,15 +21,9 @@ public class UserController {
   }
 
   @GetMapping("/me")
-  public ResponseEntity<UserDetailResponse> getUserInfo(@CurrentUser Long userId) {
-    UserDetailResponse response =  userService.getUser(userId);
-    return ResponseEntity.ok(response);
+  public Mono<ResponseEntity<UserDetailResponse>> getUserInfo(@CurrentUser String userId) {
+    Mono<UserDetailResponse> response =  userService.getUser(userId);
+    return response.map(ResponseEntity::ok);
   }
-
-//  @PutMapping("/me")
-//  public ResponseEntity<UserDetailResponse> updateUserInfo(@Valid @RequestBody UpdateRequest request, @CurrentUser Long userId) {
-//    return userService.update(request, userId)
-//        .map(ResponseEntity::ok);
-//  }
 
 }
