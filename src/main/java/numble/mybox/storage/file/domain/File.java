4c1c65df998141f883114c2bbba80519f7ddeb18
beomsic <qjatjr29@khu.ac.kr>
2023-04-30 16:18:48 +0900

    :recycle: refactor: file entity -> document 수정

--- src/main/java/numble/mybox/storage/file/domain/File.java
+++ src/main/java/numble/mybox/storage/file/domain/File.java
@@ -1,57 +1,44 @@
 package numble.mybox.storage.file.domain;
 
-import java.math.BigDecimal;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import numble.mybox.common.domain.BaseEntity;
-import numble.mybox.storage.folder.domain.Folder;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import numble.mybox.common.domain.BaseDocument;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-@Entity
-@Table(name = "file")
-@Where(clause = "is_deleted = false")
-@SQLDelete(sql = "UPDATE file SET is_deleted = true WHERE id = ?")
+@Document(collection = "file")
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class File extends BaseEntity {
+public class File extends BaseDocument {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private String id;
 
-  @Column(name = "file_name")
+  @Field(name = "file_name")
   private String fileName;
 
-  @Column(name = "file_size")
-  private BigDecimal fileSize;
+  @Field(name = "file_size")
+  private Long fileSize;
 
-  @Column(name = "file_url")
+  @Field(name = "file_url")
   private String fileUrl;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "folder_id")
-  private Folder folder;
+  @Field(name = "user_id")
+  private String userId;
 
-  @Column(name = "is_deleted")
+  @Field(name = "folder_id")
+  private String folderId;
+
+  @Field(name = "is_deleted")
   @Builder.Default
   private Boolean isDeleted = Boolean.FALSE;
 
   // TODO: 중요표시
-  // TODO: fileURL
 
 }
