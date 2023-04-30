3fa8010a58351c0b9258089670274f4baeb7e4c4
beomsic <qjatjr29@khu.ac.kr>
2023-04-30 15:39:37 +0900

    :fire: remove: @EnableJpaAuditing 사용하지 않는 어노테이션 삭제

--- src/main/java/numble/mybox/MyboxApplication.java
+++ src/main/java/numble/mybox/MyboxApplication.java
@@ -4,10 +4,8 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class MyboxApplication {
 
 	@PostConstruct
