d100de3860d6b2ec9feea4719523631ee607386c
beomsic <qjatjr29@khu.ac.kr>
2023-04-30 16:04:42 +0900

    :recycle: refactor: reactiveMongoRepository 를 사용하도록 변경

--- src/main/java/numble/mybox/storage/folder/domain/FolderRepository.java
+++ src/main/java/numble/mybox/storage/folder/domain/FolderRepository.java
@@ -1,10 +1,18 @@
 package numble.mybox.storage.folder.domain;
 
-import java.util.List;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FolderRepository extends JpaRepository<Folder, Long> {
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-  Optional<Folder> findByUserIdAndParentFolderId(Long userId, Long parentFolderId);
+public interface FolderRepository extends ReactiveMongoRepository<Folder, String> {
+
+  @Query("{'userId' : ?0, 'isRoot' : true}")
+  Mono<Folder> findByUserIdAndIsRoot(String userId);
+
+  Mono<Folder> findByIdAndUserId(String id, String userId);
+
+  Flux<Folder> findAllByParentFolderId(String parentFolderId);
+
 }
