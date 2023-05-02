016b0a91f52a2f3d448f13395c1a5ee2c5374f26
beomsic <qjatjr29@khu.ac.kr>
2023-05-02 13:07:50 +0900

    :recycle: refactor: 커스텀 예외가 ResponseStatusException를 상속받도록 변경

--- src/main/java/numble/mybox/common/error/exception/BusinessException.java
+++ src/main/java/numble/mybox/common/error/exception/BusinessException.java
@@ -1,21 +1,27 @@
 package numble.mybox.common.error.exception;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import numble.mybox.common.error.ErrorCode;
 import numble.mybox.common.error.ErrorField;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Getter
-public class BusinessException extends RuntimeException {
+public class BusinessException extends ResponseStatusException {
 
   private final ErrorCode errorCode;
   private List<ErrorField> errors;
 
   public BusinessException(ErrorCode errorCode) {
+    super(HttpStatus.valueOf(errorCode.getStatusCode()), errorCode.getMessage());
     this.errorCode = errorCode;
+    this.errors = new ArrayList<>();
   }
 
   public BusinessException(ErrorCode errorCode, List<ErrorField> errors) {
+    super(HttpStatus.valueOf(errorCode.getStatusCode()), errorCode.getMessage());
     this.errorCode = errorCode;
     this.errors = errors;
   }
