0ff06673b0c2ce3a5fe5c43088a8835a6464eb8e
beomsic <qjatjr29@khu.ac.kr>
2023-05-06 00:09:13 +0900

    :sparkles: feat: 파일 다운로드 로직 추가

--- src/main/java/numble/mybox/storage/file/presentation/FileController.java
+++ src/main/java/numble/mybox/storage/file/presentation/FileController.java
@@ -1,14 +1,22 @@
 package numble.mybox.storage.file.presentation;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
 import numble.mybox.common.presentation.CurrentUser;
 import numble.mybox.storage.file.application.FileDetailResponse;
 import numble.mybox.storage.file.application.FileService;
 import numble.mybox.storage.file.application.FileSummaryResponse;
+import numble.mybox.storage.file.domain.File;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +25,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
+@Slf4j
 @RestController
 @RequestMapping("/files")
 public class FileController {
@@ -34,6 +44,25 @@ public class FileController {
       @PathVariable String folderId,
       @RequestPart("file") FilePart file) {
     return fileService.uploadFile(userId, folderId, file).map(ResponseEntity::ok);
+  }
+
+  @GetMapping("/download/{fileId}")
+  public Mono<ResponseEntity<Resource>> downloadFile(@CurrentUser String userId,
+      @PathVariable String fileId,
+      ServerHttpResponse response) {
+    Mono<Tuple2<File, Resource>> tuple2Mono = fileService.downloadFile(userId, fileId);
+
+    return tuple2Mono.flatMap(tuple2 -> {
+          File t1 = tuple2.getT1();
+          Resource t2 = tuple2.getT2();
+          HttpHeaders headers = response.getHeaders();
+          headers.set(HttpHeaders.CONTENT_DISPOSITION,
+              "attachment;filename=" + encodeFileName(t1.getFileName()));
+          headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+          headers.set(HttpHeaders.CONTENT_LENGTH, String.valueOf(t1.getFileSize()));
+          return Mono.just(t2);
+        })
+    .map(res -> ResponseEntity.ok().headers(response.getHeaders()).body(res));
   }
 
   @GetMapping("/folder/{folderId}")
@@ -56,6 +85,15 @@ public class FileController {
   public Mono<ResponseEntity<Void>> deleteFile(@CurrentUser String userId, @PathVariable String fileId) {
     return fileService.deleteFile(userId, fileId)
         .then(Mono.just(ResponseEntity.noContent().build()));
+  }
+
+
+  private String encodeFileName(String fileName) {
+    try {
+      return URLEncoder.encode(fileName, StandardCharsets.UTF_8).replace("+", "%20");
+    } catch (Exception e) {
+      return fileName;
+    }
   }
 
 }
